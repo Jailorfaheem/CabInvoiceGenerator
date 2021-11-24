c77f40ea79bc90a49261c3b3d28748799a136fc6
faheemjailor <faheemjailor0055>
2021-11-24 22:54:34 +0530

    Faheem UC1 for Total fare of CabInvoice

---
+++ CabInvoiceGenerator/CabInvoiceTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CabInvoiceGenerator;
+
+namespace CabInvoiceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //UC1 : Test case for checking total fare function
+        [TestMethod]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            //AAA method
+            //Arrange
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double expected = 25;
+
+
+            //Act
+            double totalFare = invoiceGenerator.CaculateFare(distance, time);
+
+            //Assert
+            Assert.AreEqual(expected, totalFare);
+        }
+    }
+}
