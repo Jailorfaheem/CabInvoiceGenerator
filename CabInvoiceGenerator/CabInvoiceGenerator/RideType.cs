43388ce5f4936b5d3ae92b6c0477569fe0ee709b
faheemjailor <faheemjailor0055>
2021-11-24 23:26:45 +0530

    Faheem UC3 for invoice summary

--- CabInvoiceGenerator/CabInvoiceGenerator/RideType.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator/RideType.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CabInvoiceGenerator.CabInvoiceGenerator
+namespace CabInvoiceGenerator
 {
-    class RideType
+    public enum RideType
     {
+        NORMAL,
+        PREMIUM
     }
 }
